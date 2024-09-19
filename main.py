5c0b1321c2e5190c97c4c84f8295d4617586aa48
Suraj Gupta <surajgupta@Surajs-MacBook-Air.local>
2024-09-19 12:21:07 +0530

    commision

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
 import infmidi
 from infmidi import *
 
+class scriptdetection():
+    def __init__(self) -> None:
+        self.timesignature = float()
+        self.tempo = int()
+        self.arpeggiation = bool()
+        self.octavechords = bool()
+        self.arpeggiationtempo = int()
+        self.notesperchord = int()
+        self.treblenotesinput = list()
+        self.scalepertreblenote = list() #find
+        self.bassnotes = list() #create
+        self.duetsinchorus = bool()
+        self.basseffect = int()
+        self.basseffectchords = int()
+        
+    def findscale(self):
+        pass
+    
+    def createchords(self):
+        pass
+    
+    def arpeggiation(self):
+        pass
+    
+    def doubleoctavetreble(self):
+        pass
+    
+    def sectionslicing(self):
+        pass
+        #to differentiate the chorus, prechorus n all
